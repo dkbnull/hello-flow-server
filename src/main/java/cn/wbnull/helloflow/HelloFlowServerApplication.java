1d8e983a6286c3ca92f8d406756e4cfdfc336b03
dkbnull <dkbnull@163.com>
2026-05-24 21:59:39 +0800

    Initial commit

---
+++ src/main/java/cn/wbnull/helloflow/HelloFlowServerApplication.java
@@ -0,0 +1,13 @@
+package cn.wbnull.helloflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFlowServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloFlowServerApplication.class, args);
+    }
+
+}
